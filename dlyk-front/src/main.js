287d18b1c625b056bd905a3435b8fb2d85532255
huanzizhao <578708409@qq.com>
2024-03-18 18:43:41 +0800

    element组件国际化（中文）

--- dlyk-front/src/main.js
+++ dlyk-front/src/main.js
@@ -8,6 +8,9 @@ import { createApp } from 'vue'
 // import ... from ... 语句导入，从element-plus框架导入ElementPlus组件
 import ElementPlus from 'element-plus'
 
+// 导入element-plus的中文语言包
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
 // 导入element-plus的css样式，不需要from子句
 import 'element-plus/dist/index.css'
 
@@ -29,6 +32,6 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 
 // 利用上面所导入的creatApp()函数 创建一个vue应用，mount是挂载到#app地方
-app.use(ElementPlus).use(router).mount('#app')
+app.use(ElementPlus,{locale:zhCn}).use(router).mount('#app')
 
 
